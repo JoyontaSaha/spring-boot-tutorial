c0932826bb922295881e87aed6861b904412ed70
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-15 00:21:30 +0600

    not blank hibernet validation implemented for departementName field

--- src/main/java/com/joyonta/springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/com/joyonta/springboot/tutorial/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import com.joyonta.springboot.tutorial.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -14,7 +15,7 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @PostMapping("/saveDepartment")
-    public Department saveDepartment (@RequestBody Department department) {
+    public Department saveDepartment (@Valid @RequestBody Department department) {
         return departmentService.saveDepartment(department);
     }
 
--- src/main/java/com/joyonta/springboot/tutorial/entity/Department.java
+++ src/main/java/com/joyonta/springboot/tutorial/entity/Department.java
@@ -4,12 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+
+    @NotBlank(message = "Department name cannot be empty. Add a department name.")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
