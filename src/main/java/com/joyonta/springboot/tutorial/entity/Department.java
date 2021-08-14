92563d8c9fe981c1bd0a6c1976d48c17e0a49f61
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-15 01:15:53 +0600

    all boiler plates replaced by lombok annotations

--- src/main/java/com/joyonta/springboot/tutorial/entity/Department.java
+++ src/main/java/com/joyonta/springboot/tutorial/entity/Department.java
@@ -1,6 +1,9 @@
 package com.joyonta.springboot.tutorial.entity;
 
-import org.hibernate.validator.constraints.Length;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,10 @@ import javax.persistence.Id;
 import javax.validation.constraints.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -31,45 +38,5 @@ public class Department {
     private String departmentAddress;
     private String departmentCode;
 
-    public Long getDepartmentId() {
-        return departmentId;
-    }
 
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
 }
