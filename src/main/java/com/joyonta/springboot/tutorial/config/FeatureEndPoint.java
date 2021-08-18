e4260eb957e5820ec432bd00c40bad0452392b68
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-18 20:09:06 +0600

    Department, User, Authentication custom actuator endpoints created and implemented

---
+++ src/main/java/com/joyonta/springboot/tutorial/config/FeatureEndPoint.java
@@ -0,0 +1,41 @@
+package com.joyonta.springboot.tutorial.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Endpoint(id = "features")
+public class FeatureEndPoint {
+    private Map<String, Feature> featureMap = new ConcurrentHashMap<>();
+
+    public FeatureEndPoint() {
+        this.featureMap.put("Department", new Feature(true));
+        this.featureMap.put("User", new Feature(false));
+        this.featureMap.put("Authentication", new Feature(false));
+    }
+
+    @ReadOperation
+    public Map<String, Feature> features() {
+        return featureMap;
+    }
+
+    @ReadOperation
+    public Feature feature(@Selector String featureName) {
+        return featureMap.get(featureName);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class Feature {
+        private boolean isEnabled;
+    }
+}
